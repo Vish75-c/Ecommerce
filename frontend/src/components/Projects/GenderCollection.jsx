9332ee9c77d283cf53a0bc7a66e86d9a225838cf
Vish75-c <vishgola191@gmail.com>
2025-10-07 18:29:53 +0530

    fixedherobugs

--- frontend/src/components/Projects/GenderCollection.jsx
+++ frontend/src/components/Projects/GenderCollection.jsx
@@ -4,17 +4,17 @@ import womenImg from '../../assets/womens-collection.webp'
 import { Link } from 'react-router-dom'
 const GenderCollection = () => {
   return (
-    <section className='py-16 px-16 w-full'>
+    <section className='py-16 px-4 md:px-16 w-full'>
         <div className='container mx-auto grid grid-cols-1 md:grid-cols-2  gap-8'>
             <div className='relative flex-1'>
-                <img src={womenImg} alt="women-img" className='w-full h-[700px] object-cover rounded-md' />
+                <img src={womenImg} alt="women-img" className='w-full h-[550px] md:h-[700px] object-cover rounded-md' />
                 <div className='absolute bottom-8 left-8 rounded-md bg-white bg-opacity-80 p-4'>
                     <h1 className='text-2xl font-bold text-gray-900 mb-3'>Women's Collection</h1>
                     <Link to='/collections/all?gender=Women' className="text-gray-900 underline">Shop Now</Link>
                 </div>
             </div>
             <div className='relative flex-1'>
-                <img src={menImg} alt="men-img" className='w-full h-[700px] object-cover rounded-md' />
+                <img src={menImg} alt="men-img" className='w-full h-[550px] md:h-[700px] object-cover rounded-md' />
                 <div className='absolute bottom-8 left-8 rounded-md bg-white bg-opacity-80 p-4'>
                     <h1 className='text-2xl font-bold text-gray-900 mb-3'>Men's Collection</h1>
                     <Link to='/collections/all?gender=Men' className="text-gray-900 underline">Shop Now</Link>
